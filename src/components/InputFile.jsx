4d3f6ddd0824307fdb4b63da8fac589145cf0a20
federico <federicoilluminato@hotmail.com>
2024-08-01 01:40:49 -0300

    ui components

---
+++ src/components/InputFile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+
+const InputFile = () => {
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Input id="picture" type="file" />
+    </div>
+  )
+}
+
+export default InputFile
