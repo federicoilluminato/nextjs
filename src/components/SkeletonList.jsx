939cfc9bb1f75a1e9dd1e9c4dae0b0cccb28a5d9
federico <federicoilluminato@hotmail.com>
2024-08-01 18:05:46 -0300

    skeleton update

--- src/components/SkeletonList.jsx
+++ src/components/SkeletonList.jsx
@@ -2,11 +2,12 @@ import { Skeleton } from "@/components/ui/skeleton"
  
 export function SkeletonList() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+    <div className="flex flex-col space-y-4 w-full">
+      <div className="flex flex-col space-y-2">
+        <Skeleton className="h-12 w-full rounded-md" />
+        <Skeleton className="h-12 w-full rounded-md" />
+        <Skeleton className="h-12 w-full rounded-md" />
+        <Skeleton className="h-12 w-full rounded-md" />
       </div>
     </div>
   )
