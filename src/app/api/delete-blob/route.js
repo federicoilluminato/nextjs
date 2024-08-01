5c0ca8db82fdac4ef99c09c7b621458289ae4dd1
federico <federicoilluminato@hotmail.com>
2024-08-01 15:54:19 -0300

    delete-blob route updated

--- src/app/api/delete-blob/route.js
+++ src/app/api/delete-blob/route.js
@@ -1,9 +1,19 @@
 import { del } from '@vercel/blob';
  
 export async function DELETE(request) {
-  const { searchParams } = new URL(request.url);
-  const urlToDelete = searchParams.get('url');
-  await del(urlToDelete);
- 
-  return new Response();
+  try{
+    const { searchParams } = new URL(request.url);
+    const urlToDelete = searchParams.get('url');
+    await del(urlToDelete);
+    }
+    catch(error){
+        return new Response(JSON.stringify({ error: 'Failed to delete blob' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    return new Response(JSON.stringify({ success: true }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
 }
